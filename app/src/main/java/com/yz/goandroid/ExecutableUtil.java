ba1034d5b9e703a405f60ae950c6b345d1333767
pan <panseng.dr@qq.com>
2024-04-28 20:48:40 +0800

    初步实现Android app调用go程序的测试

--- app/src/main/java/com/yz/goandroid/ExecutableUtil.java
+++ app/src/main/java/com/yz/goandroid/ExecutableUtil.java
@@ -16,15 +16,19 @@ public class ExecutableUtil {
 
     private static final String TAG = "ExecutableUtil";
     private static final String EXECUTABLE_NAME = "android.x86_64"; // 替换为你的可执行文件名
-    private static final String TMP_ROOT = "/data/local/tmp"; // 可改变权限的tmp dir
+    private static String root_dir = ""; //
 //    private static final String EXECUTABLE_ROOT = "/data/local/tmp/yz"; // 目录
-    private static String EXECUTABLE_PATH = TMP_ROOT + "/" + EXECUTABLE_NAME; // 执行程序
+    private static String executable_path = root_dir + "/" + EXECUTABLE_NAME; // 执行程序
 
     private static String executable_data_dir = ""; // 执行程序数据目录
 
     public static void extractAndRunExecutable(Context context) {
         try {
-            executable_data_dir = context.getDataDir().getAbsolutePath() +"/data";
+            root_dir = context.getDataDir().getAbsolutePath();
+            executable_data_dir = root_dir +"/data";
+            executable_path = root_dir + "/" + EXECUTABLE_NAME;
+            CommandExecution.execCommand("chmod 777 -R " + root_dir, true);
+            
             // 创建目录
             makeDir(context);
             File data = new File(executable_data_dir);
@@ -37,18 +41,18 @@ public class ExecutableUtil {
 
             // 复制assets目录下的可执行文件到设备的内部存储
             copyExecutableFromAssetsToInternalStorage(context);
-            File outputFile = new File(EXECUTABLE_PATH);
+            File outputFile = new File(executable_path);
             if (!outputFile.exists()){
                 Log.i(TAG, "文件不存在");
                 return;
             }
 
             // 使可执行文件具有执行权限
-            makeExecutable(EXECUTABLE_PATH);
+            makeExecutable(executable_path);
 
 
             // 执行可执行文件
-            executeCommand(EXECUTABLE_PATH);
+            executeCommand(executable_path);
 
         } catch (IOException | InterruptedException e) {
             Toast.makeText(context, "出现错误: `" + e, Toast.LENGTH_SHORT).show();
@@ -59,7 +63,7 @@ public class ExecutableUtil {
     // 复制assets目录下的可执行文件到设备的内部存储
     private static void copyExecutableFromAssetsToInternalStorage(Context context) throws IOException {
         // 检查文件是否已经存在
-        File outputFile = new File(EXECUTABLE_PATH);
+        File outputFile = new File(executable_path);
         if (!outputFile.exists()) {
             Log.i(TAG, "开始复制文件");
             // 从assets复制到内部存储
@@ -74,21 +78,23 @@ public class ExecutableUtil {
             fos.flush();
             fos.close();
             fis.close();
-            Log.i(TAG,"可执行文件复制完成: " + EXECUTABLE_PATH);
+            Log.i(TAG,"可执行文件复制完成: " + executable_path);
         } else{
             Log.i(TAG, "文件已存在");
         }
     }
 
     // 执行权限
-    private static void makeExecutable(String path) throws IOException {
+    private static void makeExecutable(String path) {
         Log.i(TAG, "开始授权");
+        CommandExecution.execCommand("chmod 777 " + path, true);
+
         // 设置可执行权限
-        String command = "chmod 777 " + path;
-        DataOutputStream os = new DataOutputStream(Runtime.getRuntime().exec("sh").getOutputStream());
-        os.writeBytes(command);
-        os.flush();
-        os.close();
+//        String command = "chmod 777 " + path;
+//        DataOutputStream os = new DataOutputStream(Runtime.getRuntime().exec("sh").getOutputStream());
+//        os.writeBytes(command);
+//        os.flush();
+//        os.close();
         Log.i(TAG, "授予权限: "+path);
     }
 
